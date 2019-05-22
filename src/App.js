59864c226e0532d473e2da1722302022994889e5
TALLuff <talluff@gmail.com>
2019-05-22 12:14:01 +0100

    Writing a file in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import writeFileP from "write-file-p";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends React.Component {
+  state = {
+    download: false,
+    "200": ["/apples.html", "/salmon.html", "/oranges.html"],
+    "404": ["/stilton.html", "/garbage.html"]
+  };
+
+  componentDidUpdate() {
+    if (this.state.download === true) {
+      return (
+        <a href="./testWrite.txt" download>
+          Download
         </a>
-      </header>
-    </div>
-  );
+      );
+    }
+  }
+
+  writeAndDownload = () => {
+    var textToWrite = document.getElementById("inputTextToSave").value;
+    var textFileAsBlob = new Blob([textToWrite], { type: "text/plain" });
+    var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs")
+      .value;
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+      // Chrome allows the link to be clicked
+      // without actually adding it to the DOM.
+      downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+      // Firefox requires the link to be added to the DOM
+      // before it can be clicked.
+      downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+      downloadLink.onclick = destroyClickedElement;
+      downloadLink.style.display = "none";
+      document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+  };
+
+  sendUrl = () => {
+    // writeFileP(
+    //   "./testWrite.txt",
+    //   `200: ${this.state["200"]}, 400: ${this.state["400"]}`,
+    //   (err, data) => {
+    //     console.log(err || data);
+    //   }
+    // );
+    this.setState({ download: true });
+  };
+
+  downloadButton = () => {
+    return <a />;
+  };
+
+  render() {
+    return (
+      <body className="App">
+        <h1>Web Crawler</h1>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            this.sendUrl();
+          }}
+        >
+          <input placeholder="Enter the URL you want to check" />
+          <button type="submit">Search</button>
+        </form>
+        {this.downloadButton}
+      </body>
+    );
+  }
 }
 
 export default App;
